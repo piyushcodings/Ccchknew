67fc255970697b202d02d77353d18348c4e60427
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2023-09-21 04:52:27 +0530

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -72,7 +72,7 @@ def Tele(ccx):
 	    headers=headers,
 	    data=data,
 	).json()
-            print(response)
+                print(response)
 	try:
 		ii=response['validation_feedback']
 	except:
